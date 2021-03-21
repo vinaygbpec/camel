bd03f59e58d853de04968426b7e6bfb8714a1975
vinaygbpec <vinay@pixeltechlab.com>
2021-03-21 18:30:40 +0530

    active mq

---
+++ src/main/java/com/pixel/camel/routes/FileRoute.java
@@ -0,0 +1,14 @@
+package com.pixel.camel.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileRoute extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("file:files/input")
+                .log("${body}")
+                .to("file:files/output");
+    }
+}
--- src/main/java/com/pixel/camel/routes/MyCamelRoute.java
+++ src/main/java/com/pixel/camel/routes/MyCamelRoute.java
@@ -1,17 +1,23 @@
 package com.pixel.camel.routes;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Component
-public class MyCamelRoute extends RouteBuilder{
+public class MyCamelRoute extends RouteBuilder {
+    private Logger logger = LoggerFactory.getLogger(MyCamelRoute.class);
 
     @Override
     public void configure() throws Exception {
-       from("timer:first-timer")
-       .to("log:first-timer");
-        
+       // from("timer:first-timer").transform().constant("Time now:" + LocalDateTime.now()).
+         //       process(new SimpleProcessor())
+         //       .to("log:first-timer");
+
     }
-    
+
 
 }
